5ddcfda1926aee71a94a8379e531d55e059f9558
27Onion Nebell <57032603+onion108@users.noreply.github.com>
2022-05-22 21:49:16 +0800

    initing

---
+++ src/game/game_main.rs
@@ -0,0 +1,9 @@
+pub mod game;
+
+pub struct Game;
+
+pub impl Game {
+    pub fn run_game() {
+        println!("Hello world!");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+/**
+ * main.rs
+ * 
+ * Created by 27Onion, May 22, 2022.
+ * 
+ * The entry point of the game.
+ * 
+ */
+
+mod game;
+
 fn main() {
-    println!("Hello, world!");
+    game::Game.run_game();
 }
